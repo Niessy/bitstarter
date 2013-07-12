4e853d81358e3a586106f575fec41dd125f65842
Dominique Luna <dluna132@gmail.com>
2013-07-12 18:14:19 -0400

    Completed URL checking in grader.js, added restler package

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
 
@@ -34,22 +35,47 @@ var checkHtmlFile = function( htmlfile, checksfile ) {
     return out;
 };
 
+// hack
 var clone = function( fn ) {
     return fn.bind({});
 };
+
+var checkURL = function( url, checksfile ) {
+    rest.get(url).on('complete', function( result, response ) {
+        if (result instanceof Error) {
+            console.error("Url %s no good!", url);
+        } else {
+           $ = cheerio.load(result); 
+           var checks = loadChecks( checksfile ).sort();
+           var out = {};
+           for (var ii in checks) {
+               var present = $(checks[ii]).length > 0;
+               out[checks[ii]] = present;
+            }
+            var outJson = JSON.stringify( out, null, 4 );
+            console.log(outJson);
+        }
+    });
+};
+
 
 if ( require.main == module ) {
     program
         .option( '-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT )
         .option( '-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT )
+        .option('-u, --url <url_link>', 'URL to Check')
         .parse( process.argv );
-    var checkJson = checkHtmlFile( program.file, program.checks );
-    var outJson = JSON.stringify( checkJson, null, 4 );
-    console.log( outJson );
+    if ( !program.url ) {
+        // we have a file
+        var checkJson = checkHtmlFile( program.file, program.checks );
+        var outJson = JSON.stringify( checkJson, null, 4 );
+        console.log(outJson);
+    } else {
+        checkURL( program.url, program.checks );
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkURL = checkURL;
 }
-
-
 
 
